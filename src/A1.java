3762377f5c90b5b51a94d291a8a930a1eeb63e0e
Thomas Bandy <61776913+T-H-0-M@users.noreply.github.com>
2024-08-05 16:36:38 +1000

    Updated

--- src/A1.java
+++ src/A1.java
@@ -4,7 +4,7 @@ package COMP.Compiler;
  * Hello world!
  *
  */
-public class App {
+public class A1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
