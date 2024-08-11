0b26b1dfaac7e7d1fbf6eb41536fa368ece15c85
T_H_0_M <thomasbandy23@gmail.com>
2024-08-11 22:01:19 +1000

    COM-14 array list for returned tokens

--- src/A1.java
+++ src/A1.java
@@ -1,25 +1,38 @@
+import java.util.ArrayList;
+
 public class A1 {
+
+    private static ArrayList<Token> tokenList = new ArrayList<>();
 
     public static void main(String[] args) {
         String filePath = args[0];
-
         TokenTerminator tokenTerminator = new TokenTerminator(filePath);
-
         run(tokenTerminator);
+        printTokenList();
     }
 
     // NOTE: For debug
     private static void run(TokenTerminator tokenTerminator) {
         Token currentToken = tokenTerminator.getNextToken();
         while (true) {
-            System.out.println(currentToken.toString());
             currentToken = tokenTerminator.getNextToken();
+            tokenList.add(currentToken);
             if (currentToken.getTokenId() == 0) {
-                System.out.println("Breaking");
                 break;
             }
         }
 
     }
 
+    /**
+     * Prints all tokens in the tokenList.
+     * Each token is printed on a new line with its index in the list.
+     */
+    private static void printTokenList() {
+        System.out.println("\n--- All Tokens in tokenList ---");
+        for (int i = 0; i < tokenList.size(); i++) {
+            System.out.printf("%d: %s%n", i, tokenList.get(i).toString());
+        }
+        System.out.println("--- End of tokenList ---\n");
+    }
 }
